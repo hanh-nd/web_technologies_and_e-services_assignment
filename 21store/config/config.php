e47782dad122671a9950fd49af3ca95235382f6d
hanhnd <hanhnd156@gmail.com>
2022-06-26 23:46:30 +0700

    add base project

---
+++ 21store/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/** Configuration Variables **/
+
+const DEVELOPMENT_ENVIRONMENT = true;
+
+const DB_NAME = 'mymvc';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_HOST = 'localhost:3306';
