bf34fa4d99fe75344c37acaaf2cd35d7b745ada0
Hoàng Anh <82218114+hoanganhak53@users.noreply.github.com>
2022-07-01 00:01:04 +0700

    css profile user

--- 21store/mvc/views/profile.php
+++ 21store/mvc/views/profile.php
@@ -2,31 +2,44 @@
 
 <!DOCTYPE html>
 <html lang="vi">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="public/css/about.css" type="text/css">
-		<link rel="stylesheet" href="public/css/footer.css" type="text/css">
-		<link rel="stylesheet" href="public/css/nav_bar.css" type="text/css">
-		<title>Profile</title>
-	</head>
-	<body>
-		<?php require_once ROOT . DS . 'mvc' . DS . 'views' . DS . 'nav_bar.php'; ?>
-		<div class="about-container" >
-            <div class="about-content">
-                <div class="introduction">
-                    <h1>Giới thiệu</h1>
-                    <p>Thành lập vào tháng 6 năm 2022, 21Strore đã khẳng định được chỗ đứng vững chắc trên thị trường và trở thành thương hiệu thời trang có tốc độ tăng trưởng nhanh nhất tại Việt Nam với hệ thống cửa hàng với gần 90 showrooms trên toàn quốc; Hàng triệu khách hàng thân thiết; Là nhà bán số 1 về ngành hàng thời trang trên các sàn Thương mại điện tử phổ biến nhất trong nước như: Tiki, Lazada, Shopee, Sendo,… và sắp có mặt tại sàn Thương Mại điện tử lớn nhất Thế Giới Amazon.</p>
-                    <q>21STORE PHÁT TRIỂN MÔ HÌNH SIÊU THỊ THỜI TRANG HẠNH PHÚC</q>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/profile.css" type="text/css">
+    <link rel="stylesheet" href="public/css/footer.css" type="text/css">
+    <link rel="stylesheet" href="public/css/nav_bar.css" type="text/css">
+    <title>Profile</title>
+</head>
+
+<body>
+    <?php require_once ROOT . DS . 'mvc' . DS . 'views' . DS . 'nav_bar.php'; ?>
+    <div class="profile-container">
+        <div>
+            <h2>Hoang Anh</h2>
+            <a>Tài khoản của tôi</a><br>
+            <a>Đổi mật khẩu</a><br>
+            <a>Đơn mua</a><br>
+        </div>
+        <div class="infor-profile">
+            <header>
+                <h3>Hồ Sơ Của Tôi</h3>
+                <p>Quản lý thông tin hồ sơ để bảo mật tài khoản</p>
+                <div class="divider"></div>
+            </header>
+            <div class="content-profile">
+                <p><label>Tên Đăng Nhập</label> &emsp;<span>hoanganh</span></p>
+                <div class="attribute" style="margin-left: 18px;">
+                    <label>Số điện thoại&emsp;</label>
+                    <input type="text" class="input-text">
                 </div>
-                <div class="images">
-                    <div>
-                        <img src=<?php echo "/" . $path_project . "/" . "images/about/for-women.jpg" ?> width="1120">
-                        <img src=<?php echo "/" . $path_project . "/" . "images/about/for-men.jpg" ?> width="1120">
-                        <img src=<?php echo "/" . $path_project . "/" . "images/about/for-teen.jpg" ?> width="1120">
-                        <br><br>
-                    </div>
+                <div class="attribute" style="margin-left: 65px;">
+                    <label>Địa chỉ&emsp;</label>
+                    <input type="text" class="input-text">
                 </div>
+                <input type="submit" value="Lưu thay đổi">            
             </div>
-		</div>
+        </div>
+    </div>
+</body> 
