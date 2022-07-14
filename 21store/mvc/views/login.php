b24c9d67f71fd1a0346d01c0b60dc9e23f3123bf
GaCon404 <82218114+hoanganhak53@users.noreply.github.com>
2022-07-14 23:19:09 +0700

    validate login

--- 21store/mvc/views/login.php
+++ 21store/mvc/views/login.php
@@ -2,7 +2,6 @@
 require_once ROOT . DS . 'services' . DS . 'UserService.php';
 ?>
 
-
 <?php 
 	$userService = new UserService();
 	if (isset($_POST['password']) && isset($_POST['username'])) {
@@ -33,13 +32,32 @@ require_once ROOT . DS . 'services' . DS . 'UserService.php';
 	<div class="login-page">
 		<img src="https://cdn.haitrieu.com/wp-content/uploads/2021/07/Hinh-anh-fast-fashion-la-gi.png" alt="img">
 		<div class="form">
-			<form class="login-form" method="POST">
+			<form class="login-form" method="post" name="formLogin" onsubmit="return validateLogin()">
 				<input type="text" placeholder="username" name="username"/>
 				<input type="password" placeholder="password" name="password"/>
+				<p id="validateLogin"></p>
 				<button type="submit">login</button>
 				<p class="message">Not registered? <a href="#">Create an account</a></p>
 			</form>
 		</div>
 	</div>
 </body>
-<script type="text/javascript" src=<?php echo "/" . $path_project . "/" . "public/js/login.js" ?>></script>
+
+<script>
+    function validateLogin() {
+    const x = document.forms["formLogin"]["username"].value;
+    const y = document.forms["formLogin"]["password"].value;
+    if (x === "" || y == "") {
+        document.getElementById("validateLogin").innerHTML = "Hãy điền đủ thông tin !!";
+        return false;
+    }
+    if (x.length < 5) {
+        document.getElementById("validateLogin").innerHTML = "Tên đăng nhập phải lớn hơn 4 ký tự !!";
+        return false;
+    }
+    if (y.length < 6) {
+        document.getElementById("validateLogin").innerHTML = "Mật khẩu phải nhiều hơn 6 kí tự!!";
+        return false;
+    }
+}
+</script>
